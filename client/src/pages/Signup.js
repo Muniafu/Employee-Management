352af4bc338e6eff346c2521cfa2d758e8b17bcc
Muniafu <elambaraka2019@gmail.com>
2024-10-08 10:30:48 +0300

    added .css

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Signup.css';
+import { Link } from 'react-router-dom';
 
 function Signup() {
   const [name, setName] = useState('');
@@ -21,15 +23,19 @@ function Signup() {
   };
 
   return (
-    <div>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
+    <div className="signup">
+      <h2>Sign up</h2>
+      <form className="signup-form" onSubmit={handleSubmit}>
         <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <input type="text" placeholder="Goals" value={goals} onChange={(e) => setGoals(e.target.value)} required />
         <input type="text" placeholder="Progress" value={progress} onChange={(e) => setProgress(e.target.value)} required />
         <input type="number" placeholder="Evaluation Score" value={evaluationScore} onChange={(e) => setEvaluationScore(e.target.value)} required />
-        <button type="submit">Signup</button>
+
+        <p>
+          <Link to="/login" className="link">Already have an account?</Link>
+        </p>
+        <button className="sign-btn" type="submit">Signup</button>
       </form>
     </div>
   );
