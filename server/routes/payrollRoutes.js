adb33d08bf7ff6da66e0ec1331cae13da771de94
Muniafu <elambaraka2019@gmail.com>
2025-09-13 13:01:46 +0300

    `Removed payrollController.getPayrollForEmployee call in favor of payrollController.getMyPayrolls in /me route.`

--- server/routes/payrollRoutes.js
+++ server/routes/payrollRoutes.js
@@ -4,7 +4,6 @@ const payrollController = require('../controllers/payrollController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 // Employee self-service
-router.get('/me', protect, (req, res) => payrollController.getPayrollForEmployee(req, res));
 router.get('/me', protect, payrollController.getMyPayrolls);
 
 // Admin-only
